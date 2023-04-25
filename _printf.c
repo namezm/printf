8330f55e48dbe3ca18cbdfbeb74aee9a7685c9d3
namezm <namez.mohamed@gmail.com>
2023-04-25 11:49:08 -0700

    task 1

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *_printf - produces output according to a format
+ *By namezm & bouilla
+ *@format: character string
+ *Return: number of characters printed
+ *
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0, cp = 0, 
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <ubistd.h>
+
+int _printf(const char *format, ...);
+int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
+int ev_print_func(const char *s, int index);
+unsigned int handl_str(char *str, char c, unsigned int indexstr);
+int print_str(char *str, unsigned int nbrstr);
+
+#endif
